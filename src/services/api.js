fd2a79808dcc73bedc2232e1000c8109dba284df
Vinícius Ventura <vini.ventura999@gmail.com>
2023-04-05 15:20:09 -0300

    api config

---
+++ src/services/api.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+// Rodar com IPV4: json-server --watch -d 180 --host 192.168.0.100 db.json
+
+const api = axios.create({
+  baseURL: "http://192.168.0.100:3000",
+});
+
+export default api;
