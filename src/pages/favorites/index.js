21f83178d993553ee8c077cf830fcf0b0aaffbc8
Vinícius Ventura <vini.ventura999@gmail.com>
2023-04-06 20:11:58 -0300

    favorite tab complete

--- src/pages/favorites/index.js
+++ src/pages/favorites/index.js
@@ -1,16 +1,61 @@
-import { View, Text, StyleSheet} from "react-native" 
+import { View, Text, StyleSheet, SafeAreaView, FlatList } from "react-native";
+import { useState, useEffect } from "react";
+import { getFavorites } from "../../utils/storage";
+import { useIsFocused } from "@react-navigation/native";
+import { FoodCard } from "../../components/foodCard";
 
 export function Favorites() {
-    return (
-        <View style={styles.container}>
-            <Text>Favorites</Text>
-        </View>
-    )
+  const [receipes, setReceipes] = useState([]);
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    let isActive = true;
+
+    async function getReceipes() {
+      const result = await getFavorites("@appreceitas");
+      if (isActive) {
+        setReceipes(result);
+      }
+    }
+
+    if (isActive) {
+      getReceipes();
+    }
+
+    return () => {
+      isActive = false;
+    };
+  }, [isFocused]);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Receitas Favoritas</Text>
+      {receipes.length === 0 && (
+        <Text>Você não possui nenhuma receita salva!</Text>
+      )}
+
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        style={{ marginTop: 14 }}
+        data={receipes}
+        keyExtractor={(item) => String(item.id)}
+        renderItem={({ item }) => <FoodCard data={item}/>}
+      />
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      backgroundColor: "yellow",
-    }
-  });
-  
+  container: {
+    flex: 1,
+    backgroundColor: "#F3F9FF",
+    paddingStart: 14,
+    paddingEnd: 14,
+    paddingTop: 36,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#000",
+  },
+});
