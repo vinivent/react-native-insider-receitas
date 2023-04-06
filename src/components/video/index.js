519efe94fc2184e376e6fb27f0b07e04bb56b31d
Vinícius Ventura <vini.ventura999@gmail.com>
2023-04-06 19:06:25 -0300

    creating video component

---
+++ src/components/video/index.js
@@ -0,0 +1,9 @@
+import {View, Text, TouchableOpacity} from "react-native"
+
+export function VideoView() {
+    return (
+        <View>
+            <Text>Testando</Text>
+        </View>
+    )
+}
